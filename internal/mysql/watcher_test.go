2f4192a821f3b7b49cc389bbc3f9bc4537b187f1
Naofumi Okamoto <nokamoto.engr@gmail.com>
2021-04-28 17:51:35 +0900

    Add mysql watcher create (#5)

---
+++ internal/mysql/watcher_test.go
@@ -0,0 +1,109 @@
+package mysql
+
+import (
+	"errors"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/go-cmp/cmp"
+	"github.com/nokamoto/egosla/api"
+	"google.golang.org/protobuf/testing/protocmp"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func TestNewWatcher(t *testing.T) {
+	expected := &api.Watcher{
+		Name: "foo",
+		Keywords: []string{"bar", "baz"},
+	}
+
+	w := NewWatcher(expected)
+
+	actual := w.Value()
+
+	if diff := cmp.Diff(expected, actual, protocmp.Transform()); len(diff) != 0 {
+		t.Error(diff)
+	}
+}
+
+func TestPersistentWatcher_Create(t *testing.T) {
+	query := func(mock sqlmock.Sqlmock) *sqlmock.ExpectedExec {
+		return mock.
+			ExpectExec(regexp.QuoteMeta("INSERT INTO `watchers` (`name`,`keywords`) VALUES (?,?)")).
+			WithArgs("foo", "bar,baz")
+	}
+
+	input := &api.Watcher {
+		Name: "foo",
+		Keywords: []string{"bar", "baz"},
+	}
+
+	testcases := []struct {
+		name string
+		mock func(mock sqlmock.Sqlmock)
+		expected error
+	}{
+		{
+			name: "ok",
+			mock: func(mock sqlmock.Sqlmock) {
+				mock.ExpectBegin()
+				query(mock).WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+			},
+			expected: nil,
+		},
+		{
+			name: "unexpected error",
+			mock: func(mock sqlmock.Sqlmock) {
+				mock.ExpectBegin()
+				query(mock).WillReturnError(errors.New("unexpected"))
+				mock.ExpectRollback()
+			},
+			expected: ErrUnknown,
+		},
+	}
+
+	for _, x := range testcases {
+		t.Run(x.name, func(t *testing.T) {
+			gdb, mock, err := sqlmock.New()
+			if err != nil {
+				t.Error(err)
+			}
+			defer gdb.Close()
+
+			db, err := gorm.Open(
+				mysql.Dialector{
+					Config: &mysql.Config{
+						DriverName: "mysql", 
+						Conn: gdb, 
+						SkipInitializeWithVersion: true,
+					},
+				}, 
+				&gorm.Config{},
+			)
+			if err != nil {
+				t.Error(err)
+			}
+
+			p := &PersistentWatcher{
+				db: db,
+			}
+
+			if x.mock != nil {
+				x.mock(mock)
+			}
+
+			actual := p.Create(input)
+
+			if !errors.Is(actual, x.expected) {
+				t.Errorf("expected %v but actual %v", x.expected, actual)
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
