90be9f6e22405f602e2c1ce818957b3fe285a7d8
Naofumi Okamoto <nokamoto.engr@gmail.com>
2021-05-10 15:02:03 +0900

    Replace mock function with rpc call (#18)

--- web/src/Rpc.tsx
+++ web/src/Rpc.tsx
@@ -1,3 +1,15 @@
-export function createWatch(keywords: string[]) {
-  console.log("keywords", keywords);
+import { WatcherServiceClient } from "./api/ServiceServiceClientPb";
+
+function gitpodExposedURL(): string {
+  return (
+    window.location.protocol +
+    "//" +
+    window.location.hostname.replace("3000", "8080")
+  );
 }
+
+export const watcherService = new WatcherServiceClient(
+  gitpodExposedURL(),
+  null,
+  null
+);
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import Paperbase from "./Paperbase";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
     <Paperbase />
   </React.StrictMode>,
   document.getElementById("root")
