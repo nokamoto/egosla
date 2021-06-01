73ee33ed7d6ceb7f0c7408dd02890dc94e27f15f
Naofumi Okamoto <nokamoto.engr@gmail.com>
2021-06-01 10:05:05 +0000

    Add web subscription table

--- web/src/subscriptions/SubscriptionContent.tsx
+++ web/src/subscriptions/SubscriptionContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import {
   createStyles,
@@ -7,6 +7,10 @@ import {
   WithStyles,
 } from "@material-ui/core/styles";
 import StandardAppBar from "src/standard/StandardAppBar";
+import { ListSubscriptionRequest, Subscription } from "src/api/subscription_pb";
+import { subscriptionService } from "src/Rpc";
+import StandardTable from "src/standard/StandardTable";
+import { TableCell, TableRow } from "@material-ui/core";
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -22,6 +26,17 @@ interface contentProps extends WithStyles<typeof styles> {}
 function SubscriptionContent(props: contentProps) {
   const { classes } = props;
 
+  const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
+
+  useEffect(() => {
+    const req = new ListSubscriptionRequest();
+    req.setPageSize(100);
+    subscriptionService.listSubscription(req, {}, (err, res) => {
+      console.log(err, res);
+      setSubscriptions(res.getSubscriptionsList());
+    });
+  }, []);
+
   return (
     <Paper className={classes.paper}>
       <StandardAppBar
@@ -30,6 +45,27 @@ function SubscriptionContent(props: contentProps) {
         handleSearch={() => {}}
         searchPlaceholder="Search by name or watcher name"
         addText="Add Subscription"
+      />
+      <StandardTable
+        length={subscriptions.length}
+        visibleLength={subscriptions.length}
+        emptyTypography="No subscriptions for this workspace yet"
+        tableHeadRow={
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Watcher</TableCell>
+          </TableRow>
+        }
+        tableRows={subscriptions.map((subscription, index) => {
+          return (
+            <TableRow key={index.toString()}>
+              <TableCell component="th" scope="row">
+                {subscription.getName()}
+              </TableCell>
+              <TableCell align="right">{subscription.getWatcher()}</TableCell>
+            </TableRow>
+          );
+        })}
       />
     </Paper>
   );
