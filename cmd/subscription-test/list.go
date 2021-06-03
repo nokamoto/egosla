71e3cedcb34c9aaa774be89ec9164fe0e650785d
Naofumi Okamoto <nokamoto.engr@gmail.com>
2021-06-03 03:50:43 +0000

    Add subscription list/delete test cases

---
+++ cmd/subscription-test/list.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/nokamoto/egosla/api"
+	"github.com/nokamoto/egosla/internal/cmd/test"
+	"go.uber.org/zap"
+)
+
+func testList(c api.SubscriptionServiceClient) test.Scenario {
+	return test.Scenario{
+		Name: "ListSubscription",
+		Run: func(state test.State, logger *zap.Logger) error {
+			var expected api.Subscription
+			if err := state.Get(createdRecord, &expected); err != nil {
+				return err
+			}
+
+			return test.Until(func(ctx context.Context, pageToken string, pageSize int32) (test.ListResponse, bool, error) {
+				res, err := c.ListSubscription(ctx, &api.ListSubscriptionRequest{
+					PageToken: pageToken,
+					PageSize:  pageSize,
+				})
+				if len(res.GetSubscriptions()) != 1 {
+					return nil, false, fmt.Errorf("unexpected responnse: %v", res)
+				}
+
+				return res, test.Equal(&expected, res.GetSubscriptions()[0]) == nil, err
+			})
+		},
+	}
+}
