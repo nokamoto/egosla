7909de476fc1043632442b3241b9274c951087ef
Naofumi Okamoto <nokamoto.engr@gmail.com>
2021-06-07 12:24:17 +0900

    Implement web delete subscription (#69)

--- web/src/subscriptions/SubscriptionContent.test.tsx
+++ web/src/subscriptions/SubscriptionContent.test.tsx
@@ -3,10 +3,12 @@ import { fireEvent, render, within } from "@testing-library/react";
 import SubscriptionContent from "./SubscriptionContent";
 import { subscriptionService } from "src/Rpc";
 import {
+  DeleteSubscriptionRequest,
   ListSubscriptionRequest,
   ListSubscriptionResponse,
   Subscription,
 } from "src/api/subscription_pb";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
 
 test("gets subscriptions", () => {
   const subscription = new Subscription();
@@ -83,4 +85,57 @@ test("reload subscriptions", () => {
 
   expect(listSubscription).toHaveBeenCalledTimes(2);
   expect(listSubscription.mock.calls[1][0]).toEqual(expected);
+});
+
+test("delete subscriptions", () => {
+  const listSubscription = jest.fn().mockImplementation((x, y, callback) => {
+    const s1 = new Subscription();
+    s1.setName("foo");
+
+    const s2 = new Subscription();
+    s2.setName("bar");
+
+    const res = new ListSubscriptionResponse();
+    res.setSubscriptionsList([s1, s2]);
+    callback(null, res);
+  });
+
+  const deleteSubscription = jest.fn().mockImplementation((x, y, callback) => {
+    callback(null, new Empty());
+  });
+
+  jest
+    .spyOn(subscriptionService, "listSubscription")
+    .mockImplementation(listSubscription);
+  jest
+    .spyOn(subscriptionService, "deleteSubscription")
+    .mockImplementation(deleteSubscription);
+
+  const { getByTestId, getAllByTestId } = render(<SubscriptionContent />);
+
+  expect(listSubscription).toHaveBeenCalledTimes(1);
+  expect(deleteSubscription).toHaveBeenCalledTimes(0);
+
+  let table = getByTestId("table");
+  expect(within(table).getByText("foo")).toBeInTheDocument();
+  expect(within(table).getByText("bar")).toBeInTheDocument();
+
+  const menus = getAllByTestId("open-menu");
+  const del = getAllByTestId("delete");
+
+  expect(menus.length).toEqual(2);
+  expect(del.length).toEqual(2);
+
+  fireEvent.click(menus[0]);
+  fireEvent.click(del[0]);
+
+  const expected = new DeleteSubscriptionRequest();
+  expected.setName("foo");
+
+  expect(deleteSubscription).toHaveBeenCalledTimes(1);
+  expect(deleteSubscription.mock.calls[0][0]).toEqual(expected);
+
+  table = getByTestId("table");
+  expect(within(table).queryByText("foo")).not.toBeInTheDocument();
+  expect(within(table).getByText("bar")).toBeInTheDocument();
 });
