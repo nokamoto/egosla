ddac20eec5f22f97520cdb6101d5c39e13c384e1
Naofumi Okamoto <nokamoto.engr@gmail.com>
2021-06-15 10:44:53 +0000

    grid

--- web/src/watchers/NewWatcherContent.tsx
+++ web/src/watchers/NewWatcherContent.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { withStyles, WithStyles } from "@material-ui/core/styles";
 import contentStyles from "src/standard/contentStyles";
-import { Paper } from "@material-ui/core";
+import { Button, Grid, Paper } from "@material-ui/core";
 import ChipInput from "material-ui-chip-input";
+import { isClassExpression } from "typescript";
 
 interface contentProps extends WithStyles<typeof contentStyles> {}
 
@@ -10,22 +11,44 @@ function NewWatcherContent(props: contentProps) {
   const { classes } = props;
   return (
     <Paper className={classes.paper}>
-      <div>
-        <div className={classes.page}>
-          <ChipInput
-            label="Keywords"
-            className={classes.textField}
-            defaultValue={[]}
-            onChange={() => {}}
-            newChipKeys={[]}
-            InputProps={{
-              inputProps: {
-                "data-testid": "keywords",
-              },
-            }}
-            variant="outlined"
-          />
-        </div>
+      <div className={classes.page}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <ChipInput
+              label="Keywords"
+              defaultValue={[]}
+              onChange={() => {}}
+              newChipKeys={[]}
+              InputProps={{
+                inputProps: {
+                  "data-testid": "keywords",
+                },
+              }}
+              variant="outlined"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Button onClick={() => {}} color="secondary" data-testid="cancel">
+                Cancel
+              </Button>
+              <Button
+                onClick={() => {}}
+                color="primary"
+                variant="contained"
+                data-testid="watch"
+              >
+                Create
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     </Paper>
   );
